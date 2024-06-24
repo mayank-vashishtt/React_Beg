3752edc0551be8dd91a20ed263388df5d1bcb881
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-06-24 11:06:57 +0530

    adding addtocart and removefromcart in redux react

--- src/store.js
+++ src/store.js
@@ -4,9 +4,60 @@ function cartReducer( state  = { items : {}},action){
 
     switch(action.type){
         case "ADD_TO_CART" : { 
+            const product = action.payload;
+            if(state.items[product.id]){
+                return{
+                    ...state,
+                    items:{
+                        ...state.items,
+                        [product.id]:{
+                            ...state.items[product.id],
+                            quantity: state.items[product.id].quantity+ 1
+                        }
+                    }
+                }
+            }
+            else{
+                return{
+                    ...state,
+                    items:{
+                        ...state.items,
+                        [product.id]:{
+                            ...product,
+                            quantity: 1
+                        }
+                    }
+                }
+            }
 
         }
         case "REMOVE_FROM_CART" : {
+            const productId = action.payload.id;
+            if (!state.items[productId]) {
+                return state; // Product is not in the cart
+            }
+
+            const currentQuantity = state.items[productId].quantity;
+            if (currentQuantity > 1) {
+                return {
+                    ...state,
+                    items: {
+                        ...state.items,
+                        [productId]: {
+                            ...state.items[productId],
+                            quantity: currentQuantity - 1
+                        }
+                    }
+                };
+            } else {
+                // Remove the item completely if the quantity is 1
+                const newItems = { ...state.items };
+                delete newItems[productId];
+                return {
+                    ...state,
+                    items: newItems
+                };
+            }
 
         }
 
